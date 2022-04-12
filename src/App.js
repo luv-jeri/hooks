7dffa656e9e7949a1e012ff728598a48b3b02434
luv-jeri <luvvjeri@gmail.com>
2022-04-12 18:18:40 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,66 +1,95 @@
 import './App.css';
 import { useState, useEffect, useMemo } from 'react';
-function App() {
-  const [fee, setFee] = useState(0);
-  const [count, setCount] = useState(0);
+import React from 'react';
 
-  const data = [
-    {
-      name: 'John',
-      salary: '10000',
-      id: 1,
-    },
-    {
-      name: 'Jane',
-      salary: '20000',
-      id: 2,
-    },
-  ];
-
-  // `  intialState` is the initial state of the object if expensive to calculate
-  const [test, setTest] = useState(() => {
-    return JSON.stringify(data);
-  });
-
-  const handleFee = () => {
-    //` set State if multiple state changes
-    setFee((value) => {
-      return value + 1;
-    });
-    setFee((value) => {
-      return value + 1;
-    });
-    setFee((value) => {
-      return value + 1;
-    });
-    setFee((value) => {
-      return value + 1;
-    });
-  };
-
-  const data_ = { name: 'Sanjay', salary: '10000', id: 3 };
-  const data__ = useMemo({ name: 'Sanjay', salary: '10000', id: 3 }, []);
-
-  console.log(data__);
-
-  // const { name, salary } = data_;
-  // ______
-  // const name = data_.Sanjay;
-  // const salary = data_.salary;
-
-  useEffect(() => {}, [{ ...data }]);
-
+const Form = ({ fields }) => {
   return (
     <div>
-      <h1>Fee - {fee}</h1>
-      <button onClick={handleFee}>Click me </button>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Click Count{count}
-      </button>
+      {fields.map((field) => {
+        return (
+          <div key={field}>
+            <label>{field}</label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+// const CheckBox = ({ children }) => {
+//   const [check, setCheck] = useState(false);
+//   const all_ = React.Children.map(children, (child) => {
+//     if (typeof child.type !== 'function') {
+//       return child;
+//       // return null;
+//       // throw new Error('child must be a function');
+//     }
+//     const clone = React.cloneElement(child, {
+//       toggle: () => {
+//         console.log('toggle');
+//         setCheck(!check);
+//       },
+//       check,
+//     });
+//     return clone;
+//   });
+//   return all_;
+// };
+
+// const CheckInput = ({ toggle, check }) => {
+//   console.log('check', check);
+//   return <input checked={check} onClick={toggle} type='checkbox' />;
+// };
+
+// const Label = ({ toggle, children }) => {
+//   if (!children) return null;
+//   if (!toggle) return <span>{children}</span>;
+//   return <label onClick={toggle}>{children}</label>;
+// };
+
+// function App() {
+//   return (
+//     <CheckBox>
+//       <CheckInput />
+//       <hr />
+//       <Label>Hello</Label>
+//     </CheckBox>
+//   );
+// }
+
+// ` Declarative and Imperative
+
+// # Imperative
+const a = [1, 2, 3];
+a.push(5);  
+
+//`    pure function
+//`    no side effect
+//`    no dependency
+//`    no state
+//`    no mutable
+//`    no async
+//`    no callback
+//`    no DOM
+
+// # Declarative
+const addToArr = (array, el) => {
+  const temp = [...array, el];
+  return temp;
+};
+
+const arr = [1, 2, 3, 4, 5];
+const arr2 = addToArr(arr, 6);
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Advance Stuff
+        {arr2.map((item) => {
+          return <div key={item}>{item}</div>;
+        })}
+      </h1>
     </div>
   );
 }
